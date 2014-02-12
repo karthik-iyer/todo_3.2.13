5aae82e83663fe664fdcecf1d4cd2ade18fb9f41
Karthik <kiyer@enveritasgroup.com>
2014-02-12 18:23:31 -0500

    committed new todo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo3213::Application.config.secret_token = 'c6511a8f8cb8cdaf084cfb87afe4888ca2b700678bd63e6f0b6a443ceb1cff0cf9ec11d74bb6286cef887b0e6dcb1addb68b660e92bc6a5f67b8a9214d2f72f6'
